fd27bcab699c55a12c071fc9df8d1c7840eca94a
Shauandsss <51674001+Shauandsss@users.noreply.github.com>
2020-02-08 13:24:43 -0300

    Fixing some bugs and jQuery Problem

---
+++ controller.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    const app = require('electron').remote.app;
+    const { remote } = require('electron');
+
+
+    $('#btnCancel').click(function(){
+        console.log("Fechar");
+       remote.BrowserWindow.getFocusedWindow().close();
+    });
+
+    $('#btnPlus').click(function(){
+        console.log("Maximizar");
+       remote.BrowserWindow.getFocusedWindow().maximize();
+    });
+
+    $('#btnMinus').click(function(){
+        console.log("Minimizar");
+       remote.BrowserWindow.getFocusedWindow().minimize();
+    });
+
+
+    
+
+});
